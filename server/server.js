0a0a0e3088728defc4c128e708e4af180fa3141a
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-10-13 16:40:34 +0530

    serverless backend

--- server/server.js
+++ server/server.js
@@ -1,72 +1,63 @@
-const http = require('http');
-const { Server } = require("socket.io");
-const server = http.createServer();
-const io = new Server(server, 
-{
-    cors: 
-    {
-        origin: "http://localhost:3001", // Replace with your React app's origin
-        methods: ["GET", "POST"],
-    },
+const { Server } = require('socket.io');
+const { createServer } = require('http');
+const { VercelServer } = require('@vercel/node');
+const handler = new VercelServer(createServer());
+
+const io = new Server(handler, {
+  cors: {
+    origin: "https://tic-tac-toe-psi-two.vercel.app/", // Replace with your React app's origin
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on("connection", (socket) => 
-{
-    console.log("A user connected");
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-    // Store the joined rooms for each user
-    const joinedRooms = [];
+  // Store the joined rooms for each user
+  const joinedRooms = [];
 
-    // Handle create room
-    socket.on("create_room", (roomId) => 
-    {
-        // Leave all previously joined rooms
-        for (const room of joinedRooms) {
-        socket.leave(room);
-        }
-        socket.join(roomId);
-        joinedRooms.push(roomId);
-        console.log(`User created and joined a room: ${roomId}`);
+  // Handle create room
+  socket.on('create_room', (roomId) => {
+    // Leave all previously joined rooms
+    for (const room of joinedRooms) {
+      socket.leave(room);
+    }
+    socket.join(roomId);
+    joinedRooms.push(roomId);
+    console.log(`User created and joined a room: ${roomId}`);
+  });
+
+  // Handle new user joining room
+  socket.on('join_room', (data) => {
+    // Leave all previously joined rooms
+    for (const room of joinedRooms) {
+      socket.leave(room);
+      console.log(`User left a room: ${room}`);
+    }
+    socket.join(data.roomId);
+    joinedRooms.push(data.roomId);
+    socket.broadcast.to(data.roomId).emit('join_room', {
+      user: data.user,
+      message: 'has joined this room.',
     });
+    console.log(`User joined a room: ${data.roomId}`);
+  });
 
-    // Handle new user joining room
-    socket.on("join_room", (data) => 
-    {
-        // Leave all previously joined rooms
-        for (const room of joinedRooms) 
-        {
-            socket.leave(room);
-            console.log(`User left a room: ${room}`);
-        }
-        socket.join(data.roomId);
-        joinedRooms.push(data.roomId);
-        socket.broadcast.to(data.roomId).emit("join_room", 
-        {
-            user: data.user,
-            message: "has joined this room.",
-        });
-        console.log(`User joined a room: ${data.roomId}`);
-    });
+  // Handle send message
+  socket.on('send_message', (data) => {
+    console.log('emitting message');
+    io.to(data.roomId).emit('receive_message', { data: data });
+  });
 
-    // Handle send message
-    socket.on("send_message", (data) => 
-    {
-        console.log("emitting message");
-        io.to(data.roomId).emit("receive_message", { data: data });
-    });
-
-    socket.on("disconnect", () => 
-    {
-        console.log("A user disconnected");
-        // Leave all previously joined rooms when a user disconnects
-        for (const room of joinedRooms) {
-        socket.leave(room);
-        }
-    });
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    // Leave all previously joined rooms when a user disconnects
+    for (const room of joinedRooms) {
+      socket.leave(room);
+    }
+  });
 });
 
+handler.listen();
 
-server.listen(3000, () => 
-{
-    console.log('server is listening on port 3000');
-});
+module.exports = handler;
